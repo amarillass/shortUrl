8a18e5fd28078cf349429ee48082eff929891da4
amarillass <alejandro.amarillas@iteso.mx>
2026-04-27 19:58:01 +0200

    Update [shortId].js

--- api/[shortId].js
+++ api/[shortId].js
@@ -1,24 +1,9 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { shortId } = req.query;
 
-  try {
-    const response = await fetch(
-      `https://oz25s5y03c.execute-api.us-east-1.amazonaws.com/prod/${shortId}`,
-      { redirect: "manual" } // 🔥 clave
-    );
+  res.writeHead(302, {
+    Location: `https://oz25s5y03c.execute-api.us-east-1.amazonaws.com/prod/${shortId}`,
+  });
 
-    const location = response.headers.get("location");
-
-    if (!location) {
-      return res.status(404).send("Not found");
-    }
-
-    res.writeHead(302, {
-      Location: location,
-    });
-    res.end();
-
-  } catch (error) {
-    res.status(500).send("Error");
-  }
+  res.end();
 }
