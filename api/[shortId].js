6d59eb1c5183b11fd9afb57cb13918355e68b209
amarillass <alejandro.amarillas@iteso.mx>
2026-04-27 18:03:06 +0200

    Fetch redirect location from response headers

--- api/[shortId].js
+++ api/[shortId].js
@@ -3,19 +3,21 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://oz25s5y03c.execute-api.us-east-1.amazonaws.com/prod/${shortId}`
+      `https://oz25s5y03c.execute-api.us-east-1.amazonaws.com/prod/${shortId}`,
+      { redirect: "manual" } // 🔥 clave
     );
 
-    const data = await response.json();
+    const location = response.headers.get("location");
 
-    if (!data.originalUrl) {
+    if (!location) {
       return res.status(404).send("Not found");
     }
 
     res.writeHead(302, {
-      Location: data.originalUrl,
+      Location: location,
     });
     res.end();
+
   } catch (error) {
     res.status(500).send("Error");
   }
